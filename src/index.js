9e2ddd7e3f9cbbf7c5f5b16228dcc05033add4c8
Christian Rackerseder <git@echooff.de>
2017-11-28 20:19:16 +0100

    Changed App to Main.fullscreen();

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import './main.css';
 import { Main } from './Main.elm';
 import registerServiceWorker from './registerServiceWorker';
 
-Main.embed(document.getElementById('root'));
+Main.fullscreen();
 
 registerServiceWorker();
