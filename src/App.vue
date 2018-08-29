23b83d8da446f8471b0b58a828e233cbb0bd0139
Christian Rackerseder <git@echooff.de>
2018-08-29 07:59:46 +0200

    💡 Light bulb SVG component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,82 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="main">
+    <light-bulb />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import LightBulb from './components/LightBulb.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    LightBulb,
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css?family=Roboto');
+
+body {
+  background: #6504d4;
+  background: linear-gradient(to bottom, #6504d4, #42024b);
+  background-attachment: fixed;
+  font-family: 'Roboto', sans-serif;
+}
+
+p.errorMessage {
+  color: red;
+}
+
+svg.lightBulb {
+  height: 50vh;
+  transition: transform 0.3s;
+}
+
+svg.lightBulb:hover {
+  cursor: pointer;
+  transform: scale(1.1);
+}
+
+@keyframes power {
+  0% {
+    opacity: 1;
+  }
+  10% {
+    opacity: 1;
+  }
+  15% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+
+svg.lightBulb > .lightBulbWire {
+  animation: power 1s ease infinite;
+}
+
+footer {
+  bottom: 1rem;
+  color: white;
+  font-size: 0.7rem;
+  position: absolute;
+}
+
+footer a {
+  color: #ececec;
+}
+</style>
+
+<style scoped>
+.main {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
 }
 </style>
