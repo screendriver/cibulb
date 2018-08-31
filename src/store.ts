538c3cb8462442509913714b0ea1c75a118d1313
Christian Rackerseder <git@echooff.de>
2018-08-31 21:26:17 +0200

    🏪 Add writeValueInProgress store state field

--- src/store.ts
+++ src/store.ts
@@ -6,24 +6,33 @@ Vue.use(Vuex);
 export interface State {
   errorMessage?: string;
   deviceId?: string;
+  writeValueInProgress: boolean;
 }
 
 export enum Mutations {
   ERROR = 'error',
   CONNECTED = 'connected',
+  VALUE_WRITTEN = 'value-written',
 }
 
 export default new Vuex.Store<State>({
   state: {
     errorMessage: undefined,
     deviceId: undefined,
+    writeValueInProgress: false,
   },
   mutations: {
-    [Mutations.ERROR]: (state: State, message: string) => {
+    [Mutations.ERROR](state: State, message: string) {
       state.errorMessage = message;
     },
-    [Mutations.CONNECTED]: (state: State, deviceId: string) => {
+    [Mutations.CONNECTED](state: State, deviceId: string) {
       state.deviceId = deviceId;
+    },
+    [Mutations.VALUE_WRITTEN](state: State) {
+      state.writeValueInProgress = false;
+      // if value == getRgb Off {
+      // Bluetooth.disconnect ()
+      // }
     },
   },
   actions: {},
