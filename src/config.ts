dd37f53b652fb375222685ad7f489e66a48b5892
Christian Rackerseder <git@echooff.de>
2018-09-30 12:57:02 +0200

    ⚙ Add Config object

---
+++ src/config.ts
@@ -0,0 +1,33 @@
+export interface Config {
+  gitHub: {
+    apiUrl: string;
+    apiToken: string;
+    owner: string;
+    repos: ReadonlyArray<string>;
+  };
+}
+
+export function getConfig(env: NodeJS.ProcessEnv = process.env): Config {
+  const {
+    VUE_APP_GITHUB_API_URL,
+    VUE_APP_GITHUP_API_TOKEN,
+    VUE_APP_GITHUB_OWNER,
+    VUE_APP_GITHUB_REPOS,
+  } = env;
+  if (
+    !VUE_APP_GITHUB_API_URL ||
+    !VUE_APP_GITHUP_API_TOKEN ||
+    !VUE_APP_GITHUB_OWNER ||
+    !VUE_APP_GITHUB_REPOS
+  ) {
+    throw new Error('Environment variables are missing');
+  }
+  return {
+    gitHub: {
+      apiUrl: VUE_APP_GITHUB_API_URL,
+      apiToken: VUE_APP_GITHUP_API_TOKEN,
+      owner: VUE_APP_GITHUB_OWNER,
+      repos: VUE_APP_GITHUB_REPOS.split(','),
+    },
+  };
+}
