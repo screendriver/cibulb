32b70a9d157a7b03512864ce801e75306d0580bc
Christian Rackerseder <git@echooff.de>
2018-10-16 20:31:30 +0200

    👂 Listen on 'github' events

--- src/socket.ts
+++ src/socket.ts
@@ -1,11 +1,18 @@
 import socketIo from 'socket.io-client';
+import { Commit } from 'vuex';
+import { Mutations } from '@/store';
 
 let socket: SocketIOClient.Socket | undefined;
 
-export function connect(url: string, io = socketIo) {
+export function connect(url: string, commit: Commit, io = socketIo) {
   return new Promise((resolve, reject) => {
     socket = io(url);
-    socket.on('connect', resolve);
+    socket.on('connect', () => {
+      resolve();
+      socket!.on('github', (json: unknown) => {
+        commit(Mutations.GITHUB_HOOK_RECEIVED, json);
+      });
+    });
     socket.on('connect_error', reject);
   });
 }
