97b8b5447b81770d8b8480ed0fc9cb57a0c648d1
Christian Rackerseder <git@echooff.de>
2018-11-24 09:31:09 +0100

    🐛 Fix Firebase unit test

--- packages/browser/tests/unit/firebase.spec.ts
+++ packages/browser/tests/unit/firebase.spec.ts
@@ -12,7 +12,9 @@ describe('firebase', () => {
   it('should initialize the App', () => {
     const firebaseMock: Partial<typeof firebaseLib> = {
       initializeApp: jest.fn(),
-      messaging: jest.fn() as any,
+      messaging: jest.fn().mockReturnValue({
+        usePublicVapidKey: jest.fn(),
+      }) as any,
     };
     initializeApp(firebaseMock as typeof firebaseLib, config);
     expect(firebaseMock.initializeApp).toHaveBeenCalledWith(config);
@@ -21,9 +23,13 @@ describe('firebase', () => {
   it('should initialize messaging', () => {
     const firebaseMock: Partial<typeof firebaseLib> = {
       initializeApp: jest.fn(),
-      messaging: jest.fn() as any,
+      messaging: jest.fn().mockReturnValue({
+        usePublicVapidKey: jest.fn(),
+      }) as any,
     };
     initializeApp(firebaseMock as typeof firebaseLib, config);
-    expect(firebaseMock.messaging).toHaveBeenCalled();
+    const { messaging } = firebaseMock;
+    expect(messaging).toHaveBeenCalled();
+    expect(messaging!().usePublicVapidKey).toHaveBeenCalled();
   });
 });
