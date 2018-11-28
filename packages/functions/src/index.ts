e8112cde2fec322cb65bd90fe7c4e7eae7d4361a
Christian Rackerseder <git@echooff.de>
2018-11-28 07:39:35 +0100

    📜 Set log level to debug

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,9 +1,10 @@
 import * as functions from 'firebase-functions';
-import { Logger } from '@firebase/logger';
+import { Logger, LogLevel } from '@firebase/logger';
 
-const logger = new Logger('@firebase/cibulb-functions');
+const logger = new Logger('github');
+logger.logLevel = LogLevel.DEBUG;
 
 export const github = functions.https.onRequest((request, response) => {
-  logger.log(request);
+  logger.log(`Incoming ${request.method} request`);
   response.send(204);
 });
