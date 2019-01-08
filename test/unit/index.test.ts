da9688311a92aa2456781d87277452956764dd6a
Christian Rackerseder <git@echooff.de>
2019-01-08 22:54:34 +0100

    🗑 Remove output binding
    Because it's not needed

--- test/unit/index.test.ts
+++ test/unit/index.test.ts
@@ -4,6 +4,6 @@ import { run } from '../../ColorFunction';
 
 test('setup should work', async t => {
   t.plan(1);
-  await run({ log: sinon.stub() } as any, { query: {} } as any);
+  await run({ log: sinon.stub() } as any);
   t.pass();
 });
