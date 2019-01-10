aa2d57de463661dafc19d8cbde3300b51413dd2d
Christian Rackerseder <git@echooff.de>
2019-01-10 23:22:35 +0100

    🗑 Remove not needed test

--- test/unit/index.test.ts
+++
@@ -1,9 +0,0 @@
-import test from 'tape';
-import sinon from 'sinon';
-import { run } from '../../ColorFunction';
-
-test('setup should work', async t => {
-  t.plan(1);
-  await run({ log: sinon.stub() } as any);
-  t.pass();
-});
