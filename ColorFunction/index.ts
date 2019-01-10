9b6f5f38294a8abbc532f90f0510e912c6ffb1eb
Christian Rackerseder <git@echooff.de>
2019-01-10 23:24:17 +0100

    📞 Call changeColor() with all it's dependencies

--- ColorFunction/index.ts
+++ ColorFunction/index.ts
@@ -1,5 +1,19 @@
-import { Context } from 'azure-functions';
+import { Context, HttpRequest } from 'azure-functions';
+import verifySecret from 'verify-github-webhook-secret';
+import got from 'got';
+import { getConfig } from './config';
+import { changeColor } from './color';
 
-export async function run(context: Context) {
-  context.log('JavaScript HTTP trigger function processed a request.');
+export async function run(context: Context, req: HttpRequest) {
+  const config = getConfig();
+  const xHubSignature = req.headers['x-hub-signature'];
+  return await changeColor(
+    context.log,
+    req.body,
+    verifySecret,
+    config.githubSecret,
+    config.iftttKey,
+    got,
+    xHubSignature,
+  );
 }
