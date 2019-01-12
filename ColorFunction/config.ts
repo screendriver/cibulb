bbb5fbbb5d0b586d9dbb5d5e9fcec97c4f9db67f
Christian Rackerseder <git@echooff.de>
2019-01-12 19:10:12 +0100

    🌍 Add IFTTT base url to config object

--- ColorFunction/config.ts
+++ ColorFunction/config.ts
@@ -1,11 +1,13 @@
 export interface Config {
   githubSecret: string;
+  iftttBaseUrl: string;
   iftttKey: string;
 }
 
 export function getConfig(): Config {
   return {
     githubSecret: process.env.GITHUB_SECRET || '',
+    iftttBaseUrl: process.env.IFTTT_BASE_URL || '',
     iftttKey: process.env.IFTTT_KEY || '',
   };
 }
--- test/unit/config.test.ts
+++ test/unit/config.test.ts
@@ -4,13 +4,16 @@ import { getConfig, Config } from '../../ColorFunction/config';
 test('create config object', t => {
   t.plan(1);
   process.env.GITHUB_SECRET = 'my-secret';
+  process.env.IFTTT_BASE_URL = 'https://maker.ifttt.com';
   process.env.IFTTT_KEY = 'my-key';
   const actual = getConfig();
   const expected: Config = {
     githubSecret: 'my-secret',
+    iftttBaseUrl: 'https://maker.ifttt.com',
     iftttKey: 'my-key',
   };
   delete process.env.GITHUB_SECRET;
+  delete process.env.IFTTT_BASE_URL;
   delete process.env.IFTTT_KEY;
   t.deepEqual(actual, expected);
 });
@@ -20,6 +23,7 @@ test('initalize config object with empty strings when env variables are not set'
   const actual = getConfig();
   const expected: Config = {
     githubSecret: '',
+    iftttBaseUrl: '',
     iftttKey: '',
   };
   t.deepEqual(actual, expected);
