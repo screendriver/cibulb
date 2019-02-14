501c5ff744d02d06ce4a99553d7e62fb5baf6351
Christian Rackerseder <git@echooff.de>
2019-02-14 19:55:20 +0100

    🗑 Remove not needed logger from IFTTT

--- ColorFunction/ifttt.ts
+++ ColorFunction/ifttt.ts
@@ -1,4 +1,3 @@
-import { Logger } from '@azure/functions';
 import { GotFn } from 'got';
 import { URL } from 'url';
 import { Config } from './config';
@@ -13,15 +12,12 @@ const stateTriggerMap = {
 export async function callIftttWebhook(
   state: 'pending' | 'failure' | 'error' | 'success',
   config: Config,
-  logger: Logger,
   got: GotFn,
 ): Promise<string> {
   const iftttUrl = new URL(
     `trigger/${stateTriggerMap[state]}/with/key/${config.iftttKey}`,
     config.iftttBaseUrl,
   );
-  logger.info(`Calling IFTTT webhook with "${state}" state`);
   const { body } = await got(iftttUrl);
-  logger.info(body);
   return body;
 }
--- test/unit/ColorFunction/ifttt.test.ts
+++ test/unit/ColorFunction/ifttt.test.ts
@@ -1,7 +1,6 @@
 import test from 'tape';
 import sinon from 'sinon';
 import { GotFn } from 'got';
-import { Logger } from '@azure/functions';
 import { URL } from 'url';
 import { Config } from '../../../ColorFunction/config';
 import { callIftttWebhook } from '../../../ColorFunction/ifttt';
@@ -22,10 +21,7 @@ function createIftttUrl(state: string): URL {
 test('call IFTTT webhook with "success" state', async t => {
   t.plan(1);
   const got = sinon.stub().returns({ body: '' });
-  const logger: Partial<Logger> = {
-    info: sinon.stub(),
-  };
-  await callIftttWebhook('success', config, logger as Logger, got as GotFn);
+  await callIftttWebhook('success', config, got as GotFn);
 
   const iftttUrl = createIftttUrl('ci_build_success');
   t.true(got.calledWith(iftttUrl));
@@ -34,10 +30,7 @@ test('call IFTTT webhook with "success" state', async t => {
 test('call IFTTT webhook with "pending" state', async t => {
   t.plan(1);
   const got = sinon.stub().returns({ body: '' });
-  const logger: Partial<Logger> = {
-    info: sinon.stub(),
-  };
-  await callIftttWebhook('pending', config, logger as Logger, got as GotFn);
+  await callIftttWebhook('pending', config, got as GotFn);
 
   const iftttUrl = createIftttUrl('ci_build_pending');
   t.true(got.calledWith(iftttUrl));
@@ -46,10 +39,7 @@ test('call IFTTT webhook with "pending" state', async t => {
 test('call IFTTT webhook with "failure" state', async t => {
   t.plan(1);
   const got = sinon.stub().returns({ body: '' });
-  const logger: Partial<Logger> = {
-    info: sinon.stub(),
-  };
-  await callIftttWebhook('failure', config, logger as Logger, got as GotFn);
+  await callIftttWebhook('failure', config, got as GotFn);
 
   const iftttUrl = createIftttUrl('ci_build_failure');
   t.true(got.calledWith(iftttUrl));
@@ -58,10 +48,7 @@ test('call IFTTT webhook with "failure" state', async t => {
 test('call IFTTT webhook with "error" state', async t => {
   t.plan(1);
   const got = sinon.stub().returns({ body: '' });
-  const logger: Partial<Logger> = {
-    info: sinon.stub(),
-  };
-  await callIftttWebhook('error', config, logger as Logger, got as GotFn);
+  await callIftttWebhook('error', config, got as GotFn);
 
   const iftttUrl = createIftttUrl('ci_build_failure');
   t.true(got.calledWith(iftttUrl));
@@ -71,14 +58,6 @@ test('return IFTTT response body', async t => {
   t.plan(1);
   const body = 'Congratulations!';
   const got = sinon.stub().returns({ body });
-  const logger: Partial<Logger> = {
-    info: sinon.stub(),
-  };
-  const actual = await callIftttWebhook(
-    'success',
-    config,
-    logger as Logger,
-    got as GotFn,
-  );
+  const actual = await callIftttWebhook('success', config, got as GotFn);
   t.equal(actual, body);
 });
