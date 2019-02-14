12ae4d6c0683a4f0415755fbdab5d025cf6fe175
Christian Rackerseder <git@echooff.de>
2019-02-14 07:18:59 +0100

    ✅ Check for master branch in separate module

---
+++ test/unit/ColorFunction/branches.test.ts
@@ -0,0 +1,23 @@
+import test from 'tape';
+import { isMasterBranch } from '../../../ColorFunction/branches';
+
+test('return true when "master" branch was found', t => {
+  t.plan(1);
+  const branches = [
+    { name: 'featureA' },
+    { name: 'master' },
+    { name: 'featureB' },
+  ];
+  t.true(isMasterBranch(branches));
+});
+
+test('return false when "master" branch was not found', t => {
+  t.plan(1);
+  const branches = [{ name: 'featureA' }, { name: 'featureB' }];
+  t.false(isMasterBranch(branches));
+});
+
+test('return false when branches are empty', t => {
+  t.plan(1);
+  t.false(isMasterBranch([]));
+});
