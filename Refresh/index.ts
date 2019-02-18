3981cb4ac4218ed7dcbd3b69c1f32064f4244512
Christian Rackerseder <git@echooff.de>
2019-02-18 20:39:24 +0100

    ♻️ Convert boilerplate function to TypeScript

--- Refresh/index.ts
+++ Refresh/index.ts
@@ -1,10 +1,15 @@
-module.exports = async function(context, req) {
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+
+export const run: AzureFunction = async (
+  context: Context,
+  req: HttpRequest,
+) => {
   context.log('JavaScript HTTP trigger function processed a request.');
 
   if (req.query.name || (req.body && req.body.name)) {
     context.res = {
       // status: 200, /* Defaults to 200 */
-      body: 'Hello ' + (req.query.name || req.body.name),
+      body: 'Helloo ' + (req.query.name || req.body.name),
     };
   } else {
     context.res = {
