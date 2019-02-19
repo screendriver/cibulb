9e0ba78f9f107ff47a834f0982610a8669224b28
Christian Rackerseder <git@echooff.de>
2019-02-19 20:05:41 +0100

    🏭 Compose Refresh function

--- Refresh/index.ts
+++ Refresh/index.ts
@@ -1,20 +1,24 @@
-import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+import { AzureFunction, Context } from '@azure/functions';
+import { MongoClient } from 'mongodb';
+import got from 'got';
+import { getConfig } from '../shared/config';
+import { connect } from '../shared/mongodb';
+import { allRepositories } from './mongodb';
+import { getRepositoriesState } from '../shared/repositories';
+import { callIftttWebhook } from '../shared/ifttt';
 
-export const run: AzureFunction = async (
-  context: Context,
-  req: HttpRequest,
-) => {
-  context.log('JavaScript HTTP trigger function processed a request.');
-
-  if (req.query.name || (req.body && req.body.name)) {
-    context.res = {
-      // status: 200, /* Defaults to 200 */
-      body: 'Helloo ' + (req.query.name || req.body.name),
-    };
-  } else {
-    context.res = {
-      status: 400,
-      body: 'Please pass a name on the query string or in the request body',
-    };
-  }
+export const run: AzureFunction = async (context: Context) => {
+  const config = getConfig();
+  const mongoClient = await connect(
+    MongoClient,
+    config.mongoDbUri,
+  );
+  context.log.info('Reading all repositories from MongoDB');
+  const repositories = await allRepositories(mongoClient);
+  const overallState = getRepositoriesState(repositories);
+  context.log.info(`Calling IFTTT webhook with "${overallState}" state`);
+  const hookResponse = await callIftttWebhook(overallState, config, got);
+  context.log.info(hookResponse);
+  mongoClient.close();
+  return { status: 204, body: null };
 };
