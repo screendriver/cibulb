ee7749b463266609cca8daf391c8d28ac8a54be2
Christian Rackerseder <git@echooff.de>
2019-02-21 14:44:29 +0100

    🚢 Migrate color function from Azure to Zeit Now

--- src/color/index.ts
+++ src/color/index.ts
@@ -1,5 +1,6 @@
-import { Context, HttpRequest, AzureFunction } from '@azure/functions';
+import { IncomingMessage, ServerResponse } from 'http';
 import verifySecret from 'verify-github-webhook-secret';
+import { json } from 'micro';
 import { MongoClient } from 'mongodb';
 import got from 'got';
 import { getConfig } from '../shared/config';
@@ -11,44 +12,44 @@ import { connect } from '../shared/mongodb';
 import { updateDb } from './mongodb';
 import { getRepositoriesState } from '../shared/repositories';
 
-export const run: AzureFunction = async (
-  context: Context,
-  req: HttpRequest,
-) => {
-  const body: WebhookJsonBody = req.body;
+export default async function(req: IncomingMessage, res: ServerResponse) {
+  const body = (await json(req)) as WebhookJsonBody;
   const bodyAsString = JSON.stringify(body);
   const config = getConfig();
   const signature = xHubSignature(req);
-  context.log.info(`Called from repository ${body.name}`);
+  console.info(`Called from repository ${body.name}`);
   const isSecretValid = await verifySecret(
     bodyAsString,
     config.githubSecret,
     signature,
   );
   if (!isSecretValid) {
-    context.log.error('GitHub secret is not valid');
-    return { status: 403, body: 'Forbidden' };
+    console.error('GitHub secret is not valid');
+    res.statusCode = 403;
+    res.end('Forbidden');
+    return;
   }
   if (isWebhookJsonBody(body)) {
     if (isMasterBranch(body.branches)) {
-      context.log.info('Calling MongoDB');
+      console.info('Calling MongoDB');
       const mongoClient = await connect(
         MongoClient,
         config.mongoDbUri,
       );
       const repositories = await updateDb(mongoClient, body);
       const overallState = getRepositoriesState(repositories);
-      context.log.info(`Calling IFTTT webhook with "${overallState}" state`);
+      console.info(`Calling IFTTT webhook with "${overallState}" state`);
       const hookResponse = await callIftttWebhook(overallState, config, got);
-      context.log.info(hookResponse);
+      console.info(hookResponse);
       mongoClient.close();
     } else {
-      context.log.info(
+      console.info(
         `Called from "${body.branches
           .map(({ name }) => name)
           .toString()}" instead of "master" branch. Doing nothing.`,
       );
     }
   }
-  return { status: 204, body: null };
-};
+  res.statusCode = 204;
+  res.end();
+}
