dcf1a5647436e92c5b6382cb81195bf6e3ed8716
Christian Rackerseder <git@echooff.de>
2019-03-18 07:56:52 +0100

    📝 Require a logger when init Sentry

--- src/shared/sentry.ts
+++ src/shared/sentry.ts
@@ -1,8 +1,17 @@
-import Sentry from '@sentry/node';
+import * as Sentry from '@sentry/node';
+import { Logger } from 'loglevel';
 import { Config } from './config';
 
-export function initSentry(sentry: typeof Sentry, { sentryDSN }: Config) {
-  sentry.init({
-    dsn: sentryDSN,
-  });
+export function initSentry(
+  sentry: typeof Sentry,
+  { sentryDSN }: Config,
+  logger: Logger,
+) {
+  try {
+    sentry.init({
+      dsn: sentryDSN,
+    });
+  } catch (e) {
+    logger.error('Could not init Sentry', e);
+  }
 }
--- test/unit/shared/sentry.test.ts
+++ test/unit/shared/sentry.test.ts
@@ -1,5 +1,6 @@
 import test from 'tape';
-import Sentry from '@sentry/node';
+import * as Sentry from '@sentry/node';
+import { Logger } from 'loglevel';
 import sinon from 'sinon';
 import { initSentry } from '../../../src/shared/sentry';
 import { Config } from '../../../src/shared/config';
@@ -13,6 +14,9 @@ test('init sentry instance', t => {
   const config: Partial<Config> = {
     sentryDSN: 'https://123@sentry.io/456',
   };
-  initSentry(sentry as typeof Sentry, config as Config);
+  const logger: Partial<Logger> = {
+    error: sinon.stub(),
+  };
+  initSentry(sentry as typeof Sentry, config as Config, logger as Logger);
   t.true(init.calledWith({ dsn: 'https://123@sentry.io/456' }));
 });
