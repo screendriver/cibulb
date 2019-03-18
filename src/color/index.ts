a1d83459d3443f89479da8964a6c9a03a940b504
Christian Rackerseder <git@echooff.de>
2019-03-18 08:01:01 +0100

    ⚽️ Pass config object as dependency to color function

--- src/color/index.ts
+++ src/color/index.ts
@@ -1,6 +1,7 @@
 import log from 'loglevel';
 import { IncomingMessage, ServerResponse } from 'http';
 import { json } from 'micro';
+import { getConfig } from '../shared/config';
 import { WebhookJsonBody } from './body';
 import { xHubSignature } from './headers';
 import { run } from './run';
@@ -8,9 +9,10 @@ import { run } from './run';
 log.enableAll();
 
 export default async function(req: IncomingMessage, res: ServerResponse) {
+  const config = getConfig();
   const body = (await json(req)) as WebhookJsonBody;
   const signature = xHubSignature(req);
-  const result = await run(body, signature);
+  const result = await run(config, body, signature);
   res.statusCode = result.statusCode;
   res.end(result.text);
 }
