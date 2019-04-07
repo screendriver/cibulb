305313f3d83869fe4fc112ab55bee8f0d0b5c833
Christian Rackerseder <git@echooff.de>
2019-04-07 17:17:40 +0200

    👮 Allow master and develop branch

--- src/color/branches.ts
+++ src/color/branches.ts
@@ -2,6 +2,10 @@ export interface Branch {
   name: string;
 }
 
+const allowedBranches = ['master', 'develop'];
+
 export function isMasterBranch(branches: readonly Branch[]): boolean {
-  return branches.map(({ name }) => name).includes('master');
+  return branches
+    .map(({ name }) => name)
+    .some(name => allowedBranches.includes(name));
 }
--- test/unit/color/branches.test.ts
+++ test/unit/color/branches.test.ts
@@ -11,7 +11,27 @@ test('return true when "master" branch was found', t => {
   t.true(isMasterBranch(branches));
 });
 
-test('return false when "master" branch was not found', t => {
+test('return true when "develop" branch was found', t => {
+  t.plan(1);
+  const branches = [
+    { name: 'featureA' },
+    { name: 'develop' },
+    { name: 'featureB' },
+  ];
+  t.true(isMasterBranch(branches));
+});
+
+test('return true when "master" or "develop" branch was found', t => {
+  t.plan(1);
+  const branches = [
+    { name: 'featureA' },
+    { name: 'develop' },
+    { name: 'master' },
+  ];
+  t.true(isMasterBranch(branches));
+});
+
+test('return false when "master" or "develop" branch was not found', t => {
   t.plan(1);
   const branches = [{ name: 'featureA' }, { name: 'featureB' }];
   t.false(isMasterBranch(branches));
