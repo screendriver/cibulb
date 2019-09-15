898e70053a5328ad3d2025c7c07d2d0b160f1e4f
Christian Rackerseder <git@echooff.de>
2019-09-15 21:15:20 +0200

    🚫 Disable AVA's Babel pipeline

--- ava.config.integration.js
+++ ava.config.integration.js
@@ -1,3 +1,6 @@
+import baseConfig from './ava.config';
+
 export default {
+  ...baseConfig,
   files: ['target/test/integration/**/*.test.js'],
 };
--- ava.config.js
+++ ava.config.js
@@ -1,3 +1,5 @@
 export default {
   files: ['target/test/unit/**/*.test.js'],
+  babel: false,
+  compileEnhancements: false,
 };
