b4487abd773a387741d52ac7663011ed467e70ed
Christian Rackerseder <git@echooff.de>
2019-12-13 06:31:11 +0100

    🌎 Create AWS API gateway endpoint

--- src/cdk/stack.ts
+++ src/cdk/stack.ts
@@ -1,14 +1,19 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigw from '@aws-cdk/aws-apigateway';
 
 export class CiBulbCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new lambda.Function(this, 'RefreshHandler', {
+    const refreshHandler = new lambda.Function(this, 'RefreshHandler', {
       runtime: lambda.Runtime.NODEJS_12_X,
       code: lambda.Code.asset('target/src'),
       handler: 'refresh.handler',
+    });
+
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: refreshHandler,
     });
   }
 }
