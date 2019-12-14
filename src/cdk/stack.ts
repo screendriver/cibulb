8bcb32694ad2eee19a5a280ee3a4a2d41c2549b7
Christian Rackerseder <git@echooff.de>
2019-12-14 09:00:44 +0100

    🚫 Disable AWS API Gateway proxy

--- src/cdk/stack.ts
+++ src/cdk/stack.ts
@@ -1,10 +1,16 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
-import * as apigw from '@aws-cdk/aws-apigateway';
+import * as apigateway from '@aws-cdk/aws-apigateway';
 
 export class CiBulbCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const colorHandler = new lambda.Function(this, 'ColorHandler', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.asset('target/src'),
+      handler: 'color.handler',
+    });
 
     const refreshHandler = new lambda.Function(this, 'RefreshHandler', {
       runtime: lambda.Runtime.NODEJS_12_X,
@@ -12,8 +18,17 @@ export class CiBulbCdkStack extends cdk.Stack {
       handler: 'refresh.handler',
     });
 
-    new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: refreshHandler,
+    const api = new apigateway.LambdaRestApi(this, 'CibulbApi', {
+      handler: colorHandler,
+      proxy: false,
     });
+
+    const colorIntegration = new apigateway.LambdaIntegration(colorHandler);
+    const color = api.root.addResource('color');
+    color.addMethod('POST', colorIntegration);
+
+    const refreshIntegration = new apigateway.LambdaIntegration(refreshHandler);
+    const refresh = api.root.addResource('refresh');
+    refresh.addMethod('GET', refreshIntegration);
   }
 }
