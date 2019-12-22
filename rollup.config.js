990415e5d398c3cb2cbe3f486ed9f69e8878606b
Christian Rackerseder <git@echooff.de>
2019-12-22 13:55:41 +0100

    🏗 Enable Rollup plugin which converts JSON to ES2015 modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
 import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import json from '@rollup/plugin-json';
 import { terser } from 'rollup-plugin-terser';
 
 const plugins = [
   typescript(),
   resolve({ preferBuiltins: true }),
   commonjs(),
+  json(),
   terser(),
 ];
 const format = 'cjs';
