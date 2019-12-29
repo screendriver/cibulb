300016af143345949d085a1b5c31cc01d8adfc0b
Christian Rackerseder <git@echooff.de>
2019-12-29 01:49:39 +0100

    👀 Make CDK stack more readable

--- src/cdk/stack.ts
+++ src/cdk/stack.ts
@@ -5,52 +5,58 @@ import * as dynamodb from '@aws-cdk/aws-dynamodb';
 import * as sns from '@aws-cdk/aws-sns';
 import * as snsSubs from '@aws-cdk/aws-sns-subscriptions';
 
+const lambdaRuntime = { runtime: lambda.Runtime.NODEJS_12_X };
+const sentryDsn = { SENTRY_DSN: process.env.SENTRY_DSN ?? '' };
+const dynamoDbTableName = { DYNAMO_DB_TABLE_NAME: 'Repositories' };
+const dynamoDbPrimaryKey = { DYNAMO_DB_PRIMARY_KEY: 'Name' };
+
 export class CiBulbCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const dynamoTable = new dynamodb.Table(this, 'repositories', {
-      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
-      tableName: 'repositories',
-      readCapacity: 1,
-      writeCapacity: 1,
-      removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production code
-    });
-
-    const environment = {
-      SENTRY_DSN: process.env.SENTRY_DSN ?? '',
-      DYNAMO_DB_TABLE_NAME: dynamoTable.tableName,
-      DYNAMO_DB_PRIMARY_KEY: 'id',
-    };
-
     const colorHandler = new lambda.Function(this, 'ColorHandler', {
-      runtime: lambda.Runtime.NODEJS_12_X,
+      ...lambdaRuntime,
       code: lambda.Code.fromAsset('target/lambda/'),
       handler: 'color.handler',
       environment: {
-        ...environment,
+        ...sentryDsn,
+        ...dynamoDbTableName,
+        ...dynamoDbPrimaryKey,
         GITLAB_SECRET_TOKEN: process.env.GITLAB_SECRET_TOKEN ?? '',
       },
     });
 
     const refreshHandler = new lambda.Function(this, 'RefreshHandler', {
-      runtime: lambda.Runtime.NODEJS_12_X,
+      ...lambdaRuntime,
       code: lambda.Code.fromAsset('target/lambda/'),
       handler: 'refresh.handler',
-      environment,
+      environment: {
+        ...sentryDsn,
+        ...dynamoDbTableName,
+        ...dynamoDbPrimaryKey,
+      },
     });
 
     const iftttHandler = new lambda.Function(this, 'IftttHandler', {
-      runtime: lambda.Runtime.NODEJS_12_X,
+      ...lambdaRuntime,
       code: lambda.Code.fromAsset('target/lambda/'),
       handler: 'ifttt.handler',
       environment: {
-        SENTRY_DSN: process.env.SENTRY_DSN ?? '',
+        ...sentryDsn,
         IFTTT_BASE_URL: process.env.IFTTT_BASE_URL ?? '',
         IFTTT_KEY: process.env.IFTTT_KEY ?? '',
       },
     });
 
+    const dynamoTable = new dynamodb.Table(this, 'Repositories', {
+      partitionKey: {
+        name: dynamoDbPrimaryKey.DYNAMO_DB_PRIMARY_KEY,
+        type: dynamodb.AttributeType.STRING,
+      },
+      tableName: dynamoDbTableName.DYNAMO_DB_TABLE_NAME,
+      readCapacity: 1,
+      writeCapacity: 1,
+    });
     dynamoTable.grantReadWriteData(colorHandler);
     dynamoTable.grantReadData(refreshHandler);
 
