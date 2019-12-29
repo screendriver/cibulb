bea878982f570a7c77c9f5cb8dcadc8474845d78
Christian Rackerseder <git@echooff.de>
2019-12-29 11:22:13 +0100

    🏗 Use TypeScript project references

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,7 @@ module.exports = {
           {
             loader: 'ts-loader',
             options: {
+              projectReferences: true,
               transpileOnly: true,
             },
           },
