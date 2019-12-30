a8609c6df674f624d6b1fe79ecc1449963ae1be9
Christian Rackerseder <git@echooff.de>
2019-12-30 19:20:36 +0100

    📥 Use named instead of namespace imports

--- src/cdk/stack.ts
+++ src/cdk/stack.ts
@@ -1,27 +1,27 @@
-import * as cdk from '@aws-cdk/core';
-import * as lambda from '@aws-cdk/aws-lambda';
-import * as apigateway from '@aws-cdk/aws-apigateway';
-import * as dynamodb from '@aws-cdk/aws-dynamodb';
-import * as sns from '@aws-cdk/aws-sns';
-import * as snsSubs from '@aws-cdk/aws-sns-subscriptions';
+import { Stack, Construct, StackProps } from '@aws-cdk/core';
+import { Runtime, Function, Code } from '@aws-cdk/aws-lambda';
+import { RestApi, LambdaIntegration } from '@aws-cdk/aws-apigateway';
+import { Table, AttributeType } from '@aws-cdk/aws-dynamodb';
+import { Topic } from '@aws-cdk/aws-sns';
+import { LambdaSubscription } from '@aws-cdk/aws-sns-subscriptions';
 
-const lambdaRuntime = { runtime: lambda.Runtime.NODEJS_12_X };
+const lambdaRuntime = { runtime: Runtime.NODEJS_12_X };
 const sentryDsn = { SENTRY_DSN: process.env.SENTRY_DSN ?? '' };
 const dynamoDbTableName = { DYNAMO_DB_TABLE_NAME: 'Repositories' };
 const dynamoDbPrimaryKey = { DYNAMO_DB_PRIMARY_KEY: 'Name' };
 const topicArn = (arn: string) => ({ TOPIC_ARN: arn });
 
-export class CiBulbCdkStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class CiBulbCdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const topic = new sns.Topic(this, 'IftttTopic', {
+    const topic = new Topic(this, 'IftttTopic', {
       displayName: 'IFTTT Webhooks Topic',
     });
 
-    const colorFunction = new lambda.Function(this, 'ColorFunction', {
+    const colorFunction = new Function(this, 'ColorFunction', {
       ...lambdaRuntime,
-      code: lambda.Code.fromAsset('target/lambda/'),
+      code: Code.fromAsset('target/lambda/'),
       handler: 'color.handler',
       environment: {
         ...sentryDsn,
@@ -32,9 +32,9 @@ export class CiBulbCdkStack extends cdk.Stack {
       },
     });
 
-    const refreshFunction = new lambda.Function(this, 'RefreshFunction', {
+    const refreshFunction = new Function(this, 'RefreshFunction', {
       ...lambdaRuntime,
-      code: lambda.Code.fromAsset('target/lambda/'),
+      code: Code.fromAsset('target/lambda/'),
       handler: 'refresh.handler',
       environment: {
         ...sentryDsn,
@@ -44,9 +44,9 @@ export class CiBulbCdkStack extends cdk.Stack {
       },
     });
 
-    const iftttFunction = new lambda.Function(this, 'IftttFunction', {
+    const iftttFunction = new Function(this, 'IftttFunction', {
       ...lambdaRuntime,
-      code: lambda.Code.fromAsset('target/lambda/'),
+      code: Code.fromAsset('target/lambda/'),
       handler: 'ifttt.handler',
       environment: {
         ...sentryDsn,
@@ -55,10 +55,10 @@ export class CiBulbCdkStack extends cdk.Stack {
       },
     });
 
-    const dynamoTable = new dynamodb.Table(this, 'RepositoriesTable', {
+    const dynamoTable = new Table(this, 'RepositoriesTable', {
       partitionKey: {
         name: dynamoDbPrimaryKey.DYNAMO_DB_PRIMARY_KEY,
-        type: dynamodb.AttributeType.STRING,
+        type: AttributeType.STRING,
       },
       tableName: dynamoDbTableName.DYNAMO_DB_TABLE_NAME,
       readCapacity: 1,
@@ -67,20 +67,16 @@ export class CiBulbCdkStack extends cdk.Stack {
     dynamoTable.grantReadWriteData(colorFunction);
     dynamoTable.grantReadData(refreshFunction);
 
-    topic.addSubscription(new snsSubs.LambdaSubscription(iftttFunction));
+    topic.addSubscription(new LambdaSubscription(iftttFunction));
     topic.grantPublish(colorFunction);
     topic.grantPublish(refreshFunction);
 
-    const api = new apigateway.RestApi(this, 'CibulbApi');
+    const api = new RestApi(this, 'CibulbApi');
 
-    const colorIntegration = new apigateway.LambdaIntegration(colorFunction);
-    const color = api.root.addResource('color');
-    color.addMethod('POST', colorIntegration);
+    const colorIntegration = new LambdaIntegration(colorFunction);
+    api.root.addResource('color').addMethod('POST', colorIntegration);
 
-    const refreshIntegration = new apigateway.LambdaIntegration(
-      refreshFunction,
-    );
-    const refresh = api.root.addResource('refresh');
-    refresh.addMethod('GET', refreshIntegration);
+    const refreshIntegration = new LambdaIntegration(refreshFunction);
+    api.root.addResource('refresh').addMethod('GET', refreshIntegration);
   }
 }
