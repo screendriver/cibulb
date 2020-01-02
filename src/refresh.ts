5e2d27438c23c2b517c1254cdf734eb772c44899
Christian Rackerseder <git@echooff.de>
2020-01-02 21:07:31 +0100

    👨‍🔧 Pipe function calls in refresh lambda

--- src/refresh.ts
+++ src/refresh.ts
@@ -1,7 +1,8 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import { DocumentClient, ItemList } from 'aws-sdk/clients/dynamodb';
 import SNS from 'aws-sdk/clients/sns';
-import pino from 'pino';
+import pPipe from 'p-pipe';
+import pino, { Logger } from 'pino';
 
 export type RepositoriesStatus = 'success' | 'pending' | 'failed';
 
@@ -29,6 +30,21 @@ function isEmpty(itemList: ItemList): boolean {
   return itemList.length === 0;
 }
 
+export function logOverallStatus(logger: Logger) {
+  return (overallStatus: RepositoriesStatus): RepositoriesStatus => {
+    logger.info('Overall repositories status:', overallStatus);
+    return overallStatus;
+  };
+}
+
+export function publishSnsTopic(sns: SNS, topicArn: string) {
+  return (overallStatus: RepositoriesStatus) => {
+    return sns
+      .publish({ TopicArn: topicArn, Message: overallStatus })
+      .promise();
+  };
+}
+
 export function getRepositoriesStatus(itemList?: ItemList): RepositoriesStatus {
   return !itemList || isEmpty(itemList)
     ? 'success'
@@ -38,12 +54,13 @@ export function getRepositoriesStatus(itemList?: ItemList): RepositoriesStatus {
 export const handler: APIGatewayProxyHandler = async () => {
   const logger = pino();
   const tableName = process.env.DYNAMO_DB_TABLE_NAME ?? '';
-  const repositories = await scanRepositories(tableName);
-  const overallStatus = getRepositoriesStatus(repositories);
-  logger.info('Overall repositories status:', overallStatus);
-  await new SNS()
-    .publish({ TopicArn: process.env.TOPIC_ARN!, Message: overallStatus })
-    .promise();
+  const topicArn = process.env.TOPIC_ARN ?? '';
+  await pPipe(
+    scanRepositories,
+    getRepositoriesStatus,
+    logOverallStatus(logger),
+    publishSnsTopic(new SNS(), topicArn),
+  )(tableName);
   return {
     statusCode: 200,
     body: '',
--- test/unit/refresh.test.ts
+++ test/unit/refresh.test.ts
@@ -1,8 +1,16 @@
 import { assert } from 'chai';
 import { ItemList } from 'aws-sdk/clients/dynamodb';
-import { getRepositoriesStatus, RepositoriesStatus } from '../../src/refresh';
+import SNS from 'aws-sdk/clients/sns';
+import sinon from 'sinon';
+import { Logger } from 'pino';
+import {
+  getRepositoriesStatus,
+  RepositoriesStatus,
+  logOverallStatus,
+  publishSnsTopic,
+} from '../../src/refresh';
 
-suite.only('refresh lambda', function() {
+suite('refresh lambda', function() {
   test('getRepositoriesStatus() returns "success" when item list is undefined', function() {
     const actual = getRepositoriesStatus();
     const expected: RepositoriesStatus = 'success';
@@ -56,5 +64,40 @@ suite.only('refresh lambda', function() {
     const actual = getRepositoriesStatus(itemList);
     const expected: RepositoriesStatus = 'success';
     assert.strictEqual(actual, expected);
+  });
+
+  test('logOverallStatus() returns given overall status', function() {
+    const logger = {
+      info: sinon.fake(),
+    };
+    const actual = logOverallStatus((logger as unknown) as Logger)('success');
+    const expected: RepositoriesStatus = 'success';
+    assert.strictEqual(actual, expected);
+  });
+
+  test('logOverallStatus() logs given overall status', function() {
+    const logger = {
+      info: sinon.fake(),
+    };
+    logOverallStatus((logger as unknown) as Logger)('success');
+    sinon.assert.calledWithExactly(
+      logger.info,
+      'Overall repositories status:',
+      'success',
+    );
+  });
+
+  test('publishSnsTopic() publishes given overall status', async function() {
+    const sns = {
+      publish: sinon.fake.returns({
+        promise: sinon.fake.resolves(undefined),
+      }),
+    };
+    const topicArn = 'test';
+    await publishSnsTopic((sns as unknown) as SNS, topicArn)('success');
+    sinon.assert.calledWithExactly(sns.publish, {
+      TopicArn: topicArn,
+      Message: 'success',
+    });
   });
 });
