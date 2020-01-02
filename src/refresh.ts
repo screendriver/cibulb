9a2a967eb296ca1724d2c71d590e48f864f930a6
Christian Rackerseder <git@echooff.de>
2020-01-02 13:07:06 +0100

    ✅ Check overall repositories status in refresh lambda

--- src/refresh.ts
+++ src/refresh.ts
@@ -1,12 +1,51 @@
-import { Handler } from 'aws-lambda';
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import { DocumentClient, ItemList } from 'aws-sdk/clients/dynamodb';
 import SNS from 'aws-sdk/clients/sns';
+import pino from 'pino';
 
-export const handler: Handler = async () => {
+export type RepositoriesStatus = 'success' | 'pending' | 'failed';
+
+async function scanRepositories(tableName: string) {
+  const docClient = new DocumentClient();
+  const scanOutput = await docClient.scan({ TableName: tableName }).promise();
+  return scanOutput.Items;
+}
+
+function checkFailedStatus(itemList: ItemList): RepositoriesStatus {
+  return itemList.some(({ Status }) => Status.S === 'failed')
+    ? 'failed'
+    : 'success';
+}
+
+function getStatusForNonEmptyRepos(itemList: ItemList): RepositoriesStatus {
+  return itemList.some(
+    ({ Status }) => Status.S === 'pending' || Status.S === 'running',
+  )
+    ? 'pending'
+    : checkFailedStatus(itemList);
+}
+
+function isEmpty(itemList: ItemList): boolean {
+  return itemList.length === 0;
+}
+
+export function getRepositoriesStatus(itemList?: ItemList): RepositoriesStatus {
+  return !itemList || isEmpty(itemList)
+    ? 'success'
+    : getStatusForNonEmptyRepos(itemList);
+}
+
+export const handler: APIGatewayProxyHandler = async () => {
+  const logger = pino();
+  const tableName = process.env.DYNAMO_DB_TABLE_NAME ?? '';
+  const repositories = await scanRepositories(tableName);
+  const overallStatus = getRepositoriesStatus(repositories);
+  logger.info('Overall repositories status:', overallStatus);
   await new SNS()
-    .publish({ TopicArn: process.env.TOPIC_ARN!, Message: 'success' })
+    .publish({ TopicArn: process.env.TOPIC_ARN!, Message: overallStatus })
     .promise();
   return {
     statusCode: 200,
-    body: 'Hello World!',
+    body: '',
   };
 };
---
+++ test/unit/refresh.test.ts
@@ -0,0 +1,60 @@
+import { assert } from 'chai';
+import { ItemList } from 'aws-sdk/clients/dynamodb';
+import { getRepositoriesStatus, RepositoriesStatus } from '../../src/refresh';
+
+suite.only('refresh lambda', function() {
+  test('getRepositoriesStatus() returns "success" when item list is undefined', function() {
+    const actual = getRepositoriesStatus();
+    const expected: RepositoriesStatus = 'success';
+    assert.strictEqual(actual, expected);
+  });
+
+  test('getRepositoriesStatus() returns "success" when item list is empty', function() {
+    const itemList: ItemList = [];
+    const actual = getRepositoriesStatus(itemList);
+    const expected: RepositoriesStatus = 'success';
+    assert.strictEqual(actual, expected);
+  });
+
+  test('getRepositoriesStatus() returns "pending" when one item is in status "pending"', function() {
+    const itemList: ItemList = [
+      { Status: { S: 'failed' } },
+      { Status: { S: 'pending' } },
+      { Status: { S: 'success' } },
+    ];
+    const actual = getRepositoriesStatus(itemList);
+    const expected: RepositoriesStatus = 'pending';
+    assert.strictEqual(actual, expected);
+  });
+
+  test('getRepositoriesStatus() returns "pending" when one item is in status "running"', function() {
+    const itemList: ItemList = [
+      { Status: { S: 'failed' } },
+      { Status: { S: 'running' } },
+      { Status: { S: 'success' } },
+    ];
+    const actual = getRepositoriesStatus(itemList);
+    const expected: RepositoriesStatus = 'pending';
+    assert.strictEqual(actual, expected);
+  });
+
+  test('getRepositoriesStatus() returns "failed" when one item is in status "failed"', function() {
+    const itemList: ItemList = [
+      { Status: { S: 'success' } },
+      { Status: { S: 'failed' } },
+    ];
+    const actual = getRepositoriesStatus(itemList);
+    const expected: RepositoriesStatus = 'failed';
+    assert.strictEqual(actual, expected);
+  });
+
+  test('getRepositoriesStatus() returns "success" when all item are in status "success"', function() {
+    const itemList: ItemList = [
+      { Status: { S: 'success' } },
+      { Status: { S: 'success' } },
+    ];
+    const actual = getRepositoriesStatus(itemList);
+    const expected: RepositoriesStatus = 'success';
+    assert.strictEqual(actual, expected);
+  });
+});
