9841f8c3a4b0343d984f151c1558a04624d263ad
Christian Rackerseder <git@echooff.de>
2020-01-18 12:51:36 +0100

    👮‍♂️ Create module to verify if given branch is allowed

---
+++ test/unit/branch.test.ts
@@ -0,0 +1,28 @@
+import { assert } from 'chai';
+import { isBranchAllowed } from '../../src/branch';
+
+suite('branch', function() {
+  test('return true when branch is "master', function() {
+    assert.isTrue(isBranchAllowed('master'));
+  });
+
+  test('return true when branch is "refs/heads/master', function() {
+    assert.isTrue(isBranchAllowed('refs/heads/master'));
+  });
+
+  test('return true when branch is "develop', function() {
+    assert.isTrue(isBranchAllowed('develop'));
+  });
+
+  test('return true when branch is "refs/heads/develop', function() {
+    assert.isTrue(isBranchAllowed('refs/heads/develop'));
+  });
+
+  test('return false when branch is "foo"', function() {
+    assert.isFalse(isBranchAllowed('foo'));
+  });
+
+  test('return false when branch is an empty string', function() {
+    assert.isFalse(isBranchAllowed(''));
+  });
+});
