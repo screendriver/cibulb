5241908a0920860e64a0668d165f72b412643896
Christian Rackerseder <git@echooff.de>
2020-01-19 15:54:50 +0100

    🗑 Remove api/ directory

--- api/_color/branches.ts
+++
@@ -1,5 +0,0 @@
-const allowedBranches = ['master', 'develop'];
-
-export function isBranchAllowed(branch: string): boolean {
-  return allowedBranches.some(allowed => branch.includes(allowed));
-}
--- api/_color/headers.ts
+++
@@ -1,9 +0,0 @@
-import { IncomingMessage } from 'http';
-
-export function xGitlabToken(req: IncomingMessage): string {
-  const signature = req.headers['x-gitlab-token'];
-  if (typeof signature === 'string') {
-    return signature;
-  }
-  return '';
-}
