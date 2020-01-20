98f31bab7f9e57ccbbff4185ee4fedc23ca71500
Christian Rackerseder <git@echooff.de>
2020-01-20 17:37:00 +0100

    ✉️ Send AWS SQS message in refresh lambda function

--- src/lambda/refresh.ts
+++ src/lambda/refresh.ts
@@ -1,42 +1,16 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
-import { DocumentClient } from 'aws-sdk/clients/dynamodb';
-import SNS from 'aws-sdk/clients/sns';
-import pPipe from 'p-pipe';
-import pino, { Logger } from 'pino';
+import SQS from 'aws-sdk/clients/sqs';
 import { getEndpoint } from '../endpoint';
-import {
-  RepositoriesStatus,
-  getRepositoriesStatus,
-  scanRepositories,
-} from '../repositories';
-
-export function logOverallStatus(logger: Logger) {
-  return (overallStatus: RepositoriesStatus): RepositoriesStatus => {
-    logger.info('Overall repositories status:', overallStatus);
-    return overallStatus;
-  };
-}
-
-export function publishSnsTopic(sns: SNS, topicArn: string) {
-  return (overallStatus: RepositoriesStatus) => {
-    return sns
-      .publish({ TopicArn: topicArn, Message: overallStatus })
-      .promise();
-  };
-}
 
 export const handler: APIGatewayProxyHandler = async () => {
-  const logger = pino();
-  const tableName = process.env.DYNAMODB_TABLE_NAME ?? '';
-  const topicArn = process.env.TOPIC_ARN ?? '';
-  const docClient = new DocumentClient({ endpoint: getEndpoint(4569) });
-  const sns = new SNS({ endpoint: getEndpoint(4575) });
-  await pPipe(
-    scanRepositories(docClient),
-    getRepositoriesStatus,
-    logOverallStatus(logger),
-    publishSnsTopic(sns, topicArn),
-  )(tableName);
+  const queueUrl = process.env.QUEUE_URL ?? '';
+  const sqs = new SQS({ endpoint: getEndpoint(4576) });
+  await sqs
+    .sendMessage({
+      QueueUrl: queueUrl,
+      MessageBody: '',
+    })
+    .promise();
   return {
     statusCode: 200,
     body: '',
--- test/integration/refresh.test.ts
+++ test/integration/refresh.test.ts
@@ -1,34 +1,10 @@
 import { assert } from 'chai';
-import AWS, { Credentials, Lambda, SNS } from 'aws-sdk';
-import micro, { json } from 'micro';
-import listen from 'test-listen';
-import { IncomingMessage } from 'http';
+import AWS, { Credentials, Lambda, SQS } from 'aws-sdk';
 import { createFunction, deleteFunction } from './lambda';
-import { createDynamoDb, deleteDynamoDb } from './dynamodb';
-import { createTopic, deleteTopic } from './sns';
-
-function getHost() {
-  return process.env.HOST ?? 'host.docker.internal';
-}
-
-async function confirmSubscription(req: IncomingMessage, sns: SNS) {
-  const { TopicArn, Token } = (await json(req)) as {
-    TopicArn: string;
-    Token: string;
-  };
-  const response = await sns.confirmSubscription({ Token, TopicArn }).promise();
-  return response.SubscriptionArn;
-}
-
-async function readSnsMessage(req: IncomingMessage) {
-  const body = (await json(req)) as { Message: string };
-  return body.Message;
-}
 
 const functionName = 'refresh';
-const tableName = 'Repositories';
-const topic = 'IftttTopic';
-let topicArn: string;
+let sqs: SQS;
+let queueUrl: string;
 
 suite('refresh', function() {
   suiteSetup(async function() {
@@ -36,42 +12,29 @@ suite('refresh', function() {
       credentials: new Credentials('myAccessKeyId', 'mySecretAccessKey'),
       region: 'eu-central-1',
     });
-    topicArn = await createTopic(topic);
+    sqs = new SQS({ endpoint: 'http://localhost:4576' });
+    const queue = await sqs.createQueue({ QueueName: 'TestQueue' }).promise();
+    queueUrl = queue.QueueUrl ?? '';
     await createFunction(functionName, {
-      DYNAMODB_TABLE_NAME: tableName,
-      TOPIC_ARN: topicArn,
+      QUEUE_URL: 'http://172.17.0.1:4576/queue/TestQueue',
     });
-    await createDynamoDb(tableName);
   });
 
   suiteTeardown(async function() {
-    await deleteTopic(topicArn);
     await deleteFunction(functionName);
-    await deleteDynamoDb(tableName);
+    await sqs.deleteQueue({ QueueUrl: queueUrl }).promise();
   });
 
-  test('lambda publishes a "success" SNS message', async function() {
+  test('lambda sends a SQS message without a message body', async function() {
     this.timeout(20000);
-    let snsMessage: string | undefined = undefined;
     const lambda = new Lambda({ endpoint: 'http://localhost:4574' });
-    const sns = new SNS({ endpoint: 'http://localhost:4575' });
-    const server = micro(async req => {
-      switch (req.headers['x-amz-sns-message-type']) {
-        case 'SubscriptionConfirmation':
-          return confirmSubscription(req, sns);
-        case 'Notification':
-          snsMessage = await readSnsMessage(req);
-          return snsMessage;
-        default:
-          return '';
-      }
-    });
-    const url = await listen(server, getHost());
-    await sns
-      .subscribe({ Endpoint: url, Protocol: 'http', TopicArn: topicArn })
-      .promise();
     await lambda.invoke({ FunctionName: functionName }).promise();
-    server.close();
-    assert.strictEqual(snsMessage, 'success');
+    const message = await sqs
+      .receiveMessage({
+        QueueUrl: queueUrl,
+      })
+      .promise();
+    const messages = message.Messages ?? [];
+    assert.strictEqual(messages[0].Body, '');
   });
 });
