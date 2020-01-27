6e18b20346b54f39e03151bded2508f9f56267b1
Christian Rackerseder <git@echooff.de>
2020-01-27 11:01:36 +0100

    ✨ Create function to read AWS request tracing id

--- test/unit/headers.test.ts
+++ test/unit/headers.test.ts
@@ -1,6 +1,6 @@
 import { assert } from 'chai';
 import { APIGatewayProxyEvent } from 'aws-lambda';
-import { readGitlabToken } from '../../src/headers';
+import { readGitlabToken, readRequestTracing } from '../../src/headers';
 
 type EventHeaders = APIGatewayProxyEvent['headers'];
 
@@ -9,24 +9,24 @@ function createHeaders(overrides: EventHeaders = {}): EventHeaders {
 }
 
 suite('headers', function() {
-  test('returns an empty string when headers is undefined', function() {
+  test('readGitlabToken() returns an empty string when headers is undefined', function() {
     const actual = readGitlabToken();
     assert.strictEqual(actual, '');
   });
 
-  test('returns an empty string when headers are undefined', function() {
+  test('readGitlabToken() returns an empty string when headers are undefined', function() {
     const headers = undefined;
     const actual = readGitlabToken(headers);
     assert.strictEqual(actual, '');
   });
 
-  test('returns an empty string when "x-gitlab-token" is not present', function() {
+  test('readGitlabToken() returns an empty string when "x-gitlab-token" is not present', function() {
     const headers = createHeaders();
     const actual = readGitlabToken(headers);
     assert.strictEqual(actual, '');
   });
 
-  test('returns "x-gitlab-token" from request headers', function() {
+  test('readGitlabToken() returns "x-gitlab-token" from request headers', function() {
     const headers = createHeaders({
       'x-gitlab-token': 'foo',
     });
@@ -34,11 +34,44 @@ suite('headers', function() {
     assert.strictEqual(actual, 'foo');
   });
 
-  test('works also with case sensitive headers', function() {
+  test('readGitlabToken() works also with case sensitive headers', function() {
     const headers = createHeaders({
       'X-Gitlab-Token': 'foo',
     });
     const actual = readGitlabToken(headers);
+    assert.strictEqual(actual, 'foo');
+  });
+
+  test('readRequestTracing() returns an empty string when headers is undefined', function() {
+    const actual = readRequestTracing();
+    assert.strictEqual(actual, '');
+  });
+
+  test('readRequestTracing() returns an empty string when headers are undefined', function() {
+    const headers = undefined;
+    const actual = readRequestTracing(headers);
+    assert.strictEqual(actual, '');
+  });
+
+  test('readRequestTracing() returns an empty string when "x-amzn-trace-id" is not present', function() {
+    const headers = createHeaders();
+    const actual = readRequestTracing(headers);
+    assert.strictEqual(actual, '');
+  });
+
+  test('readRequestTracing() returns "x-amzn-trace-id" from request headers', function() {
+    const headers = createHeaders({
+      'x-amzn-trace-id': 'foo',
+    });
+    const actual = readRequestTracing(headers);
+    assert.strictEqual(actual, 'foo');
+  });
+
+  test('readRequestTracing() works also with case sensitive headers', function() {
+    const headers = createHeaders({
+      'X-Amzn-Trace-Id': 'foo',
+    });
+    const actual = readRequestTracing(headers);
     assert.strictEqual(actual, 'foo');
   });
 });
