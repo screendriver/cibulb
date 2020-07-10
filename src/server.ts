5ea3ad106427dc176bf45353ba5e9d843274fda9
Christian Rackerseder <git@echooff.de>
2020-07-10 08:53:52 +0200

    ✨ Initialize koa and middlewares

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import Koa from 'koa';
+import Router from '@koa/router';
+import koaLogger from 'koa-pino-logger';
+import bodyParser from 'koa-bodyparser';
+import pino from 'pino';
+import { color } from './routes/color';
+
+const logger = pino();
+const app = new Koa();
+const router = new Router();
+
+router.post('/color', color);
+
+app.use(koaLogger({ logger }));
+app.use(bodyParser());
+app.use(router.routes());
+app.listen(8080, () => {
+  logger.info('Server listening on port 8080');
+});
