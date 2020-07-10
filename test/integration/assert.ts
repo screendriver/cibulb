b5d86418f504ddf3edcca5ecfefba7d0d06592b3
Christian Rackerseder <git@echooff.de>
2020-07-10 11:26:17 +0200

    🗑 Remove old integration tests

--- test/integration/assert.ts
+++
@@ -1,7 +0,0 @@
-import { AssertionError } from 'assert';
-
-export function assertIsString(val?: string): asserts val is string {
-  if (typeof val !== 'string') {
-    throw new AssertionError({ message: 'Not a string!' });
-  }
-}
