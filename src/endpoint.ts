ed539738f8c2c3f1e9829495622af83b2a63efa4
Christian Rackerseder <git@echooff.de>
2020-07-13 14:23:06 +0200

    🗑 Remove not used modules

--- src/endpoint.ts
+++
@@ -1,5 +0,0 @@
-export function getEndpoint(port: number): string | undefined {
-  return process.env.LOCALSTACK_HOSTNAME
-    ? `http://${process.env.LOCALSTACK_HOSTNAME}:${port}`
-    : undefined;
-}
